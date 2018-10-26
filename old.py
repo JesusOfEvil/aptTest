b32b76c33a86d79a102f037b178978801ecd8cee
Paul Beglin <glouno@MacBook-Pro-de-Paul.local>
2018-10-26 17:11:00 +0100

    Float added

--- old.py
+++ old.py
@@ -7,7 +7,7 @@ while 1:
     line = input()
     total = 0
     for num in line.split(" "):
-        total += int(num)
+        total += float(num)
     print("Total: %d (%f%%)" % (total, total * (100.0 / out_of)))
     print('Elliots first chnage')
 	print("Enter numbers")
